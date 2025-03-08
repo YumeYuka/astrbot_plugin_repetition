6b184a34ea3df8a2b156036d30510d0adc936aac
FengYing1314 <FengYing@miao520.fun>
2025-03-08 15:14:22 +0800

    更新README，增加复读功能指令说明和配置项，修改主程序以支持复读功能的开关和打断施法概率

--- main.py
+++ main.py
@@ -8,10 +8,24 @@ import random
 
 @register("astrbot_plugin_repetition", "FengYing", "复读机插件", "1.0.1", "https://github.com/FengYing1314/astrbot_plugin_repetition")
 class RepetitionPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: dict):
         super().__init__(context)
         self.last_messages = defaultdict(list)
         self.repeat_count = defaultdict(int)
+        self.disabled_groups = set()
+        self.config = config
+        
+    @event_message_type(EventMessageType.ALL)
+    async def on_command(self, event: AstrMessageEvent):
+        '''处理开关命令'''
+        if event.message_obj.message_str == "/repetition off":
+            if event.message_obj.group_id:
+                self.disabled_groups.add(event.message_obj.group_id)
+                yield event.plain_result("已在本群关闭复读功能")
+        elif event.message_obj.message_str == "/repetition on":
+            if event.message_obj.group_id:
+                self.disabled_groups.discard(event.message_obj.group_id)
+                yield event.plain_result("已在本群开启复读功能")
 
     def get_message_identifier(self, message) -> str:
         """获取消息的唯一标识符"""
@@ -41,6 +55,10 @@ class RepetitionPlugin(Star):
         if "/" in event.message_obj.message_str:
             return
             
+        # 检查群是否已禁用
+        if event.message_obj.group_id and event.message_obj.group_id in self.disabled_groups:
+            return
+            
         session_id = event.unified_msg_origin
         current_message = event.message_obj.message
         
@@ -51,12 +69,11 @@ class RepetitionPlugin(Star):
         if message_id == self.last_messages[session_id]:
             self.repeat_count[session_id] += 1
             if self.repeat_count[session_id] == 1:
-                # 修改为30%概率复读
-                if random.random() < 0.3:
+                if random.random() < self.config.get('break_spell_probability', 0.3):
+                    yield event.plain_result(self.config.get('break_spell_text', '打断施法！'))
+                else:
                     new_chain = self.rebuild_message_chain(current_message)
                     yield event.chain_result(new_chain)
-                else:
-                    yield event.plain_result("打断施法！")
         else:
             self.repeat_count[session_id] = 0
             
